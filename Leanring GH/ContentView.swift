683a44e19c18f1cf2a9f0df3461f0cc84b106dd0
Matt <matthewjanderson89@icloud.com>
2021-07-12 12:39:19 +0100

    Initial Commit

---
+++ Leanring GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Leanring GH
+//
+//  Created by Matt Anderson on 12/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Leanring GH/Leanring_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leanring_GHApp.swift
+//  Leanring GH
+//
+//  Created by Matt Anderson on 12/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Leanring_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
